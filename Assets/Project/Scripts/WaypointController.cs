cda650203863e3cd5d59a8a2b25d40c35bcee2a9
Brandon White <brandon.white@whiteecho.com>
2014-04-27 16:31:29 +1000

    Initial Commit 27/04/2014

---
+++ Assets/Project/Scripts/WaypointController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaypointController : MonoBehaviour {
+	public GameObject waypointPrefab;
+
+	private GameObject[] waypoints;
+
+	public int ammount = 5;
+	private float largest = 100f;
+
+	// Use this for initialization
+	void Start () {
+
+		createWaypoints ();
+
+		selectWaypoint ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void createWaypoints()
+	{
+		waypoints = new GameObject[ammount];
+
+		for (int i = 0; i<ammount; i++) {
+			Vector3 pos = getRandomPos();
+
+			GameObject temp = Instantiate(waypointPrefab, pos, Quaternion.identity) as GameObject;
+
+			waypoints[i] = temp;
+
+
+			Debug.Log("Created Object at: " + pos.ToString());
+		}
+	}
+
+	Vector3 getRandomPos() {
+		float x = Random.Range (-largest, largest);
+		float z = Random.Range (-largest, largest);
+		Vector3 position = new Vector3(x, 0, z);
+
+		return position;
+	}
+
+	void selectWaypoint() {
+
+		int index = (int)Random.Range (0, ammount - 1);
+
+		waypoints [index].SendMessage ("setSelected", true);
+
+	}
+}
---
+++ Assets/Project/Scripts/Waypont.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypont : MonoBehaviour {
+
+	public string waypointName;
+
+	public bool selected = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		if(other.gameObject.tag == "Player" && selected == true)
+		{
+			selected = false;
+
+			GameObject waypointcontroler = GameObject.FindGameObjectWithTag("WaypointController");
+
+			waypointcontroler.SendMessage("selectWaypoint");
+		}
+
+
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawSphere(transform.position, 1);
+		if(selected)
+			Gizmos.color = Color.green;
+		else
+			Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere (transform.position, 10);
+	}
+
+	void setSelected(bool select)
+	{
+		selected = select;
+	}
+}
